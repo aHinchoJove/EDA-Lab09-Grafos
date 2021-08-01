12ccb9552bb88f95e3332c10a0a44b4ffdc7e7aa
aHinchoJove <82891103+aHinchoJove@users.noreply.github.com>
2021-08-01 16:27:38 -0500

    Creando la clase ListaEnlazada

---
+++ src/ListaEnlazada.java
@@ -0,0 +1,66 @@
+
+public class ListaEnlazada<T> {
+	
+	// Atributos de la clase ListaEnlazada
+	
+	protected Nodo<T> first;
+	
+	// Constructor de la clase ListaEnlazada
+	
+	public ListaEnlazada() {
+		setFirst(null);
+	}
+	
+	// Metodos setters y getters
+	
+	public void setFirst(Nodo<T> first) {
+		this.first = first;
+	}
+	
+	public Nodo<T> getFirst() {
+		return this.first;
+	}
+	
+	// Metodos de la clase Lista Enlazada
+	
+	// Metodo isEmpty verifica si la Lista Enlazada esta vacia
+	public boolean isEmpty() {
+		return getFirst() == null;
+	}
+	
+	// Insertar un Nodo al inicio
+	public void insert(T data) {
+		setFirst(new Nodo<T>(data, getFirst()));
+	}
+	
+	// Buscar un elemento dentro de nuestra Lista Enlazada
+	public T search(T data) {
+		// Rescatar la referencia al primer Nodo
+		Nodo<T> aux = getFirst();
+		// Recorrer la Lista Enlazada desde el primer Nodo
+		while(aux != null && !aux.getData().equals(data)) {
+			aux = aux.getNext();
+		}
+		// Evaluar el contenido y dar respuesta
+		if(aux != null) {
+			// Busqueda con exito
+			return aux.getData();
+		} else {
+			// Busqueda sin exito
+			return null;
+		}
+	}
+	
+	// Metodo toString, devuelve contenido de la Lista Enlazada
+	@Override
+	public String toString() {
+		String retornar = "";
+		Nodo<T> aux = getFirst();
+		while(aux != null) {
+			retornar += (aux.getData() + ", ");
+			aux = aux.getNext();
+		}
+		return retornar;
+	}
+	
+}
