475b9e5e9d135a85a489165323042a70c11e16b8
aHinchoJove <82891103+aHinchoJove@users.noreply.github.com>
2021-08-01 19:04:50 -0500

    Creando clase Test para probar clase Grafo

---
+++ src/TestGrafo.java
@@ -0,0 +1,29 @@
+
+public class TestGrafo {
+	
+  public static void main(String[] args) {
+		
+		// Creando un objeto de tipo Grafo
+		Grafo<String> grafo = new Grafo<String>();
+		
+		// Insertando algunos Vertices
+		grafo.insertarVertice("Lima");
+		grafo.insertarVertice("Arequipa");
+		grafo.insertarVertice("Cusco");
+		grafo.insertarVertice("Piura");
+		grafo.insertarVertice("Tarapoto");
+		
+		// Insertando algunas Aristas
+		grafo.insertarArista("Lima", "Arequipa");
+		grafo.insertarArista("Cusco", "Arequipa");
+		grafo.insertarArista("Piura", "Lima");
+		grafo.insertarArista("Cusco", "Lima");
+		grafo.insertarArista("Piura", "Tarapoto");
+		
+		// Imprimiendo el contenido del Grafo
+		System.out.println("Grafo de ciudades:\n");
+		System.out.println(grafo);
+		
+	}
+  
+}
