854fd68c5ff8810a8ab9a57467731e8aa0ce8aea
aHinchoJove <82891103+aHinchoJove@users.noreply.github.com>
2021-08-03 10:06:39 -0500

    Probando la comparacion incluye entre dos Listas

--- src/TestGrafo.java
+++ src/TestGrafo.java
@@ -3,6 +3,22 @@ public class TestGrafo {
 	
 	public static void main(String[] args) {
 		
+		// Lista grande
+		ListaEnlazada<Integer> grande = new ListaEnlazada<Integer>();
+		grande.insertLast(4);
+		grande.insertLast(7);
+		grande.insertLast(1);
+		grande.insertLast(2);
+		ListaEnlazada<Integer> pequena = new ListaEnlazada<Integer>();
+		pequena.insertLast(1);
+		pequena.insertLast(2);
+		pequena.insertLast(2);
+		
+		System.out.println("Comparacion : " + grande.incluye(pequena) + "\n");
+		
+		System.out.println("Lista Grande:\n" + grande);
+		System.out.println("Lista Grande:\n" + pequena);
+		
 		// Creando un objeto de tipo Grafo
 		Grafo<String> grafo = new Grafo<String>();
 		
@@ -33,6 +49,34 @@ public class TestGrafo {
 		// Probando el recorrido BFS
 		System.out.println("Probando el recorrido BFS para Arequipa");
 		grafo.BFS("Arequipa");
+		
+		// Creando otro objeto de tipo Grafo para poder ver si este
+		// incluye al creado por primera vez objeto 'grafo'
+		
+		Grafo<String> otroGrafo = new Grafo<String>();
+		
+		// Insertando algunos Vertices
+		otroGrafo.insertarVertice("Lima");
+		otroGrafo.insertarVertice("Arequipa");
+		otroGrafo.insertarVertice("Cusco");
+		otroGrafo.insertarVertice("Piura");
+		otroGrafo.insertarVertice("Tarapoto");
+		otroGrafo.insertarVertice("Moquegua");
+		otroGrafo.insertarVertice("Callao");
+		
+		// Insertando algunas Aristas
+		otroGrafo.insertarArista("Lima", "Arequipa", 8);
+		otroGrafo.insertarArista("Moquegua", "Callao", 10);
+		otroGrafo.insertarArista("Cusco", "Arequipa", 4);
+		otroGrafo.insertarArista("Piura", "Lima", 5);
+		otroGrafo.insertarArista("Lima", "Callao", 5);
+		otroGrafo.insertarArista("Callao", "Moquegua", 7);
+		otroGrafo.insertarArista("Cusco", "Lima", 6);
+		otroGrafo.insertarArista("Piura", "Tarapoto", 3);
+		
+		// Imprimiendo el contenido del otroGrafo
+		System.out.println("Otro Grafo de ciudades:\n");
+		System.out.println(otroGrafo);
 		
 	}
 	
